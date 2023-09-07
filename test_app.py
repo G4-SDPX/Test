4912b1d41c0005f36e401f297c5ae15488955916
SasiHan <Hangster10761@gmail.com>
2023-09-07 14:59:19 +0700

    []

--- test_app.py
+++ test_app.py
@@ -1,10 +1,2 @@
-
-def Revers_string(x):
-     return ' '.join([wrd[::-1] for wrd in in_wrd.split(' ')])
-
-# def test_answer(x):
-#     reversed_x = Revers_string(x)
-#     assert reversed_x == x[::-1]
-#     print("Before:", x, "After:", reversed_x)
-
-# test_answer("Hello Word")
+def reverse_word(in_wrd: str) -> str:
+    return ' '.join([wrd[::-1] for wrd in in_wrd.split(' ')])
