f878e304f1178143411e134bc7e1a22b56ce9d18
SasiHan <Hangster10761@gmail.com>
2023-09-07 15:02:09 +0700

    []

--- unit_test.py
+++ unit_test.py
@@ -4,7 +4,7 @@ from test_app import reverse_str
 class TestWord(unittest.TestCase):
     
     def test_reverse(self):
-        self.assertEqual(reverse_str(('Hello'), 'olleH'))
+        self.assertEqual(reverse_str('Hello'), 'olleH')
         self.assertEqual(reverse_str('World'), 'dlroW')
         # self.assertEqual(Revers_string("Let's take LeetCode contest"), "s'teL ekat edoCteeL tsetnoc")
 
